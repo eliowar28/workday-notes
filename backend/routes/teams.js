34be4c58dfe4bca24f1ff3848ec2e507ace0ce58
Eliomar Martinez <eliomar.martinez@gmail.com>
2020-04-12 17:11:09 -0400

    init commit workday-notes

--- backend/routes/teams.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-let teams = require('../controllers/team');
-
-router.get('/',teams.show_team);
-
-router.post('/add', teams.create_team)
-
-module.exports = router;
