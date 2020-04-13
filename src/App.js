9a2ff71b520715fa7da31ce2f21df2d662d455bd
Eliomar Martinez <eliomar.martinez@gmail.com>
2020-04-13 16:01:24 -0400

    creating first component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MyButton from './components/mybutton'
 
-function App() {
-  return (
-    <div className="workday-notes">
-      <h1>Hello Test</h1>
-    </div>
-  );
+class myComponent extends React.Component{
+  render(){
+    return(
+      <div>
+        <h5> this is my main component, below is the button</h5>
+        <MyButton/>
+      </div>
+    );
+  }
 }
 
-function Hi({name}){
-  return (
-  <div className="container">
-  <div className="card">
-    <h3>{name}</h3>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-      Laudantium quisquam eum delectus aspernatur a. Itaque dolorem 
-      nihil earum sed,aut temporibus in, velit facilis magni delectus 
-      provident ipsum excepturi! Minima?
-    </p>
-  </div>
-
-  <div className="card">
-    <h3>{name}</h3>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-      Laudantium quisquam eum delectus aspernatur a. Itaque dolorem 
-      nihil earum sed,aut temporibus in, velit facilis magni delectus 
-      provident ipsum excepturi! Minima?
-    </p>
-  </div>
-  <div className="card">
-    <h3>{name}</h3>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-      Laudantium quisquam eum delectus aspernatur a. Itaque dolorem 
-      nihil earum sed,aut temporibus in, velit facilis magni delectus 
-      provident ipsum excepturi! Minima?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-      Laudantium quisquam eum delectus aspernatur a. Itaque dolorem 
-      nihil earum sed,aut temporibus in, velit facilis magni delectus 
-      provident ipsum excepturi! Minima?
-    </p>
-</div>
-</div>
-    
-  );
-}
-
-export default Hi;
+export default myComponent;
